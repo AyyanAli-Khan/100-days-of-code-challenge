fa046fb20ca6440ad99557e92c7efae28cf68dbc
Ayyan Ali Khan <ayyanalikhan510@gmail.com>
2024-03-10 01:32:10 +0500

    updated commit

---
+++ Day_09/Alien Colors#3/main.ts
@@ -0,0 +1,13 @@
+let alien_color: string = "green";
+
+if (alien_color == "green") {
+      console.log("Player earn 5 points.");
+}
+
+else if (alien_color == "yellow"){
+    console.log("Player earn 10 points.");
+}
+
+else if (alien_color == "red"){
+    console.log("Player earn 15 points.");
+}
