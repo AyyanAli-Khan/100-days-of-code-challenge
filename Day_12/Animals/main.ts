a50b5f0fd13c961655cb8a7af8e00e2631c04e49
Ayyan Ali Khan <ayyanalikhan510@gmail.com>
2024-03-13 01:50:34 +0500

    new commit

---
+++ Day_12/Animals/main.ts
@@ -0,0 +1,9 @@
+let animals: string[] = ["cow","goat","camel","deer"];
+
+for (let index = 0; index < animals.length; index++) {
+    const currentAnimal = animals[index];
+
+    console.log(`${currentAnimal} is a innocent animal`)
+};
+
+console.log("All these animals are herbivores");
---
+++ Day_12/Pizzas/main.js
@@ -0,0 +1,7 @@
+"use strict";
+let favouritePizzas = ["cheese brust", "chicken fajita", "chicken tandori"];
+for (let index = 0; index < favouritePizzas.length; index++) {
+    const element = favouritePizzas[index];
+    console.log(`I like to eat ${element} pizza`);
+}
+console.log("I adore the mouthwatering combination of cheese burst, chicken fajita, and chicken tandoori toppings on my pizza. The rich, gooey cheese bursting out with every bite perfectly complements the flavorful chicken fajita strips and the aromatic spices of the chicken tandoori. Each slice is a delightful explosion of savory flavors that leave me craving for more. I really love to eat pizza!");
